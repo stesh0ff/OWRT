44518a904c00c9f1e6cf0c224d3139061b6e9839
stesh0ff <90890256+stesh0ff@users.noreply.github.com>
2024-10-09 01:34:45 +0300

    Update dececord.sh

--- dececord.sh
+++ dececord.sh
@@ -3,4 +3,4 @@
 wget -O /etc/config/firewall https://raw.githubusercontent.com/stesh0ff/OWRT/refs/heads/main/dis
 /etc/init.d/firewall restart
 
-echo "Firewall configuration has been replaced and firewall service restarted."
+echo "Deceda - magic, no less"
